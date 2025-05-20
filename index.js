ddf9b75fef992430d3c0589ae4b19e3d6a9e6ee8
Hamida Fathi <hamidafathi200@gmail.com>
2025-05-20 19:43:38 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,79 @@
+let numberOfElements = document.querySelectorAll(".drum").length;
+for (i = 0; i < numberOfElements; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    let buttonInnerHtml = this.innerHTML;
+    switch (buttonInnerHtml) {
+      case "w":
+        let tom1 = new Audio("./sounds/tom-1.mp3");
+        tom1.play();
+        break;
+      case "a":
+        let tom2 = new Audio("./sounds/tom-2.mp3");
+        tom2.play();
+        break;
+      case "s":
+        let tom3 = new Audio("./sounds/tom-3.mp3");
+        tom3.play();
+        break;
+      case "d":
+        let tom4 = new Audio("./sounds/tom-4.mp3");
+        tom4.play();
+        break;
+      case "j":
+        let snare = new Audio("./sounds/snare.mp3");
+        snare.play();
+        break;
+      case "k":
+        let crash = new Audio("./sounds/crash.mp3");
+        crash.play();
+        break;
+      case "l":
+        let kick = new Audio("./sounds/kick-bass.mp3");
+        kick.play();
+        break;
+
+      default:
+        console.log(buttonInnerHtml);
+    }
+  });
+}
+document.addEventListener("keypress", function (event) {
+  let keyEvent = event.key;
+  switch (keyEvent) {
+    case "w":
+      let tom1 = new Audio("./sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "a":
+      let tom2 = new Audio("./sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case "s":
+      let tom3 = new Audio("./sounds/tom-3.mp3");
+      tom3.play();
+      break;
+    case "d":
+      let tom4 = new Audio("./sounds/tom-4.mp3");
+      tom4.play();
+      break;
+    case "j":
+      let snare = new Audio("./sounds/snare.mp3");
+      snare.play();
+      break;
+    case "k":
+      let crash = new Audio("./sounds/crash.mp3");
+      crash.play();
+      break;
+    case "l":
+      let kick = new Audio("./sounds/kick-bass.mp3");
+      kick.play();
+      break;
+
+    default:
+      console.log(keyEvent);
+  }
+});
+
+// let audio = new Audio("./sounds/tom-1.mp3");
+
+// audio.play();
